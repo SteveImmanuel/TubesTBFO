89ba90369131fab183845a6d13725223303189c1
SteveImmanuel <38256664+SteveImmanuel@users.noreply.github.com>
2018-09-23 22:09:53 +0700

    almostfinal

--- mtictactoe.c
+++ mtictactoe.c
@@ -9,7 +9,7 @@ int main()
 	matrixofstring matrix_trans,finalstate;
 	char buffer[200],temp[15]="----x----";
 	sentence state,nstate;//state untuk state sekarang, nstate untuk state berikutnya
-	int k,check,input=0,rotation=0,istate,end=0,p_score=0,c_score=0;
+	int k,check,input,outinput=0,rotation=0,istate,end=0,p_score=0,c_score=0;
 
 	config =fopen("config.txt","r");//load file konfigurasi
 	if (!config) {
@@ -36,13 +36,13 @@ int main()
 		}
 	}while(check);
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	while(input!=4){
+	while(outinput!=4){
 		printf("\nTIC TAC TOE\n\n");
 		printf("Your Score : %d\nComputer's Score : %d\n\n",p_score,c_score);
 		printf("Option :\n1. You play first\n2. Computer plays first\n3. Help\n4. Exit\n");
-		printf("Your input : ");scanf("%d",&input);
-			if(input==1||input==2){
-				if (input==1){//player pertama, pergi ke state ke-13
+		printf("Your input : ");scanf("%d",&outinput);
+			if(outinput==1||outinput==2){
+				if (outinput==1){//player pertama, pergi ke state ke-13
 					strcpy(str(state),Elmt(matrix_trans,13,0));//load state
 					srand(time(0));
 					rotation=(rand()%4)*90;
@@ -50,13 +50,16 @@ int main()
 					drawboard(temp,rotation);
 				}else{//cpu pertama, pergi ke state ke-1
 					strcpy(str(state),Elmt(matrix_trans,1,0));//load state
-					printf("\nComputer's turn.\n");
-					drawboard(str(state),rotation);//jika cpu pertama, harus ditentukan ke mana player memilih agar tahu rotasinya
-					printf("Your turn.\nYour input : ");
-					scanf("%d",&input);
-					if (input==3||input==6) rotation=90;
-					else if (input==4||input==7) rotation=270;
-					else if (input==8||input==9) rotation=180;
+					do{
+						printf("\nComputer's turn.\n");
+						drawboard(str(state),rotation);//jika cpu pertama, harus ditentukan ke mana player memilih agar tahu rotasinya
+						printf("Your turn.\nYour input : ");//dan input user tidak boleh 5 karena kotak 5 sudah terisi
+						scanf("%d",&input);
+						if (input==3||input==6) rotation=90;
+						else if (input==4||input==7) rotation=270;
+						else if (input==8||input==9) rotation=180;
+						else printf("You cannot pick filled place.\n");
+					}while(input==5);					
 					input=rotateinput(input,rotation);
 					drawtemp(state, input,rotation);
 					state=transtate(state,input,matrix_trans);
@@ -94,10 +97,10 @@ int main()
 						printf("Round draw.\n");
 					}
 				}while(istate==-1);
-			}else if(input==3){
+			}else if(outinput==3){
 				printf("This is standard TIC TAC TOE games. However, should you \nchoose to go first, you must choose the middle first.\n");
-			}else if(input!=4){
-				printf("Wrong input.");
+			}else if(outinput!=4){
+				printf("Wrong input.\n");
 			}
 	}
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
